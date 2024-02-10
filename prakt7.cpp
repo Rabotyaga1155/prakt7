87c2ecdda419188987d592381ac53b5521cc8fea
Rabotyaga1155 <123551706+Rabotyaga1155@users.noreply.github.com>
2024-02-10 12:01:02 +0500

    Add files via upload

---
+++ prakt7.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+double power(double x, int n) {
+    if (n == 0) return 1.0;
+    else if (n > 0) return x * power(x, n - 1);
+    else return 1.0 / power(x, -n);
+}
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+double f(double x, int n) {
+    if (n == 0) return 1.0;
+    return power(x, n) / factorial(n);
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    double x;
+    int n;
+
+    std::cout << "Введите x ";
+    std::cin >> x;
+
+    std::cout << "Введите n ";
+    std::cin >> n;
+
+    std::cout << "f(x, n) = " << f(x, n) << std::endl;
+
+    return 0;
+}
